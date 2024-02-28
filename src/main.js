7be6b22143616b2fe11a71dd48a6f4b91a640d37
devluan777 <zcanaldedois@gmail.com>
2024-02-27 22:29:40 -0300

    primeiro commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+var numero1 = 4;
+var numero2 = 10;
+var nome = "Lívia";
+function multiplicar(numero1, numero2) { return numero1 * numero2; }
+;
+function dizOla(nome) {
+    return "olá " + nome;
+}
+console.log(multiplicar(numero1, numero2));
+console.log(dizOla(nome));
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+var numero1 = 4
+var numero2 = 10
+var nome = "Lívia"
+
+function multiplicar (numero1: number , numero2: number):number
+{return numero1 * numero2};
+
+function dizOla(nome:string): string {
+    return "olá " + nome;
+}
+
+console.log(multiplicar (numero1,numero2));
+console.log(dizOla(nome));
